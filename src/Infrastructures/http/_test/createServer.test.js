7fd78da4c3fdfcbb8e5edbd257bb3ba5fa5d2b2e
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-11-29 11:18:16 +0700

    update file

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -12,6 +12,18 @@ describe('HTTP server', () => {
     await UsersTableTestHelper.cleanTable();
   });
  
+  it('should response 404 when request unregistered route', async () => {
+    // Arrange
+    const server = await createServer({});
+    // Action
+    const response = await server.inject({
+      method: 'GET',
+      url: '/unregisteredRoute',
+    });
+    // Assert
+    expect(response.statusCode).toEqual(404);
+  });
+ 
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // Arrange
@@ -43,18 +55,21 @@ describe('HTTP server', () => {
         password: 'secret',
       };
       const server = await createServer(container);
+ 
       // Action
       const response = await server.inject({
         method: 'POST',
         url: '/users',
         payload: requestPayload,
       });
+ 
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada');
     });
+ 
     it('should response 400 when request payload not meet data type specification', async () => {
       // Arrange
       const requestPayload = {
@@ -63,18 +78,21 @@ describe('HTTP server', () => {
         fullname: ['Dicoding Indonesia'],
       };
       const server = await createServer(container);
+ 
       // Action
       const response = await server.inject({
         method: 'POST',
         url: '/users',
         payload: requestPayload,
       });
+ 
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('tidak dapat membuat user baru karena tipe data tidak sesuai');
     });
+ 
     it('should response 400 when username more than 50 character', async () => {
       // Arrange
       const requestPayload = {
@@ -83,18 +101,21 @@ describe('HTTP server', () => {
         fullname: 'Dicoding Indonesia',
       };
       const server = await createServer(container);
+ 
       // Action
       const response = await server.inject({
         method: 'POST',
         url: '/users',
         payload: requestPayload,
       });
+ 
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('tidak dapat membuat user baru karena karakter username melebihi batas limit');
     });
+ 
     it('should response 400 when username contain restricted character', async () => {
       // Arrange
       const requestPayload = {
@@ -103,18 +124,21 @@ describe('HTTP server', () => {
         fullname: 'Dicoding Indonesia',
       };
       const server = await createServer(container);
+ 
       // Action
       const response = await server.inject({
         method: 'POST',
         url: '/users',
         payload: requestPayload,
       });
+ 
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('tidak dapat membuat user baru karena username mengandung karakter terlarang');
     });
+ 
     it('should response 400 when username unavailable', async () => {
       // Arrange
       await UsersTableTestHelper.addUser({ username: 'dicoding' });
@@ -124,17 +148,42 @@ describe('HTTP server', () => {
         password: 'super_secret',
       };
       const server = await createServer(container);
+ 
       // Action
       const response = await server.inject({
         method: 'POST',
         url: '/users',
         payload: requestPayload,
       });
+ 
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('username tidak tersedia');
     });
+  });
+ 
+  it('should handle server error correctly', async () => {
+    // Arrange
+    const requestPayload = {
+      username: 'dicoding',
+      fullname: 'Dicoding Indonesia',
+      password: 'super_secret',
+    };
+    const server = await createServer({}); // fake injection
+ 
+    // Action
+    const response = await server.inject({
+      method: 'POST',
+      url: '/users',
+      payload: requestPayload,
+    });
+ 
+    // Assert
+    const responseJson = JSON.parse(response.payload);
+    expect(response.statusCode).toEqual(500);
+    expect(responseJson.status).toEqual('error');
+    expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
   });
 });
