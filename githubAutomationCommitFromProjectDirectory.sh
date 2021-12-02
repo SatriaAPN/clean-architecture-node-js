cebac5a8ee77bd9f6e37d664d4cbd3b365b0ddee
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-12-02 12:30:06 +0700

    add automation testing

---
+++ githubAutomationCommitFromProjectDirectory.sh
@@ -0,0 +1,13 @@
+ed automation.md <<'EOF'
+1i
+this is the text
+.
+w
+q
+EOF
+git config user.email "satrianusa10@gmail.com"
+git config user.name "Satria Aluh Perwira Nusa"
+git add .
+git commit -m "add automation testing"
+git push
+$SHELL
